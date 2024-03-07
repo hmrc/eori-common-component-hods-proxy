4a3bde401b4cafea220359ecc7e13bf701fc66cd
wp94 <43645818+wp94@users.noreply.github.com>
2024-03-07 17:35:20 +0000

    DDCYLS-4510 run scala formatter

--- app/uk/gov/hmrc/customs/hodsproxy/metrics/CdsMetrics.scala
+++ app/uk/gov/hmrc/customs/hodsproxy/metrics/CdsMetrics.scala
@@ -26,59 +26,41 @@ import uk.gov.hmrc.customs.hodsproxy.metrics.MetricsEnum.MetricsEnum
 class CdsMetrics @Inject() (metrics: MetricRegistry) {
 
   val timers = Map(
-    MetricsEnum.SUBSCRIBE                 -> metrics.timer("subscribe-response-timer"),
-    MetricsEnum.REGISTER_WITH_ID_MATCH    -> metrics.timer("messaging-register-with-id-timer"),
-    MetricsEnum.REGISTER_WITHOUT_ID       -> metrics.timer("messaging-register-without-id-timer"),
-    MetricsEnum.REGISTER_WITH_EORI_AND_ID -> metrics.timer("messaging-register-with-eori-timer"),
-    MetricsEnum.SUBSCRIPTION_STATUS       -> metrics.timer("messaging-subscription-status-timer"),
-    MetricsEnum.SUBSCRIPTION_DISPLAY      -> metrics.timer("messaging-subscription-display-timer"),
-    MetricsEnum.REGISTRATION_DISPLAY      -> metrics.timer("messaging-registration-display-timer"),
-    MetricsEnum.VAT_KNOWN_FACTS_CONTROL_LIST -> metrics.timer(
-      "messaging-vat-known-facts-control-list-timer"
-    ),
-    MetricsEnum.UPDATE_VERIFIED_EMAIL -> metrics.timer("messaging-update-verified-email-timer")
+    MetricsEnum.SUBSCRIBE                    -> metrics.timer("subscribe-response-timer"),
+    MetricsEnum.REGISTER_WITH_ID_MATCH       -> metrics.timer("messaging-register-with-id-timer"),
+    MetricsEnum.REGISTER_WITHOUT_ID          -> metrics.timer("messaging-register-without-id-timer"),
+    MetricsEnum.REGISTER_WITH_EORI_AND_ID    -> metrics.timer("messaging-register-with-eori-timer"),
+    MetricsEnum.SUBSCRIPTION_STATUS          -> metrics.timer("messaging-subscription-status-timer"),
+    MetricsEnum.SUBSCRIPTION_DISPLAY         -> metrics.timer("messaging-subscription-display-timer"),
+    MetricsEnum.REGISTRATION_DISPLAY         -> metrics.timer("messaging-registration-display-timer"),
+    MetricsEnum.VAT_KNOWN_FACTS_CONTROL_LIST -> metrics.timer("messaging-vat-known-facts-control-list-timer"),
+    MetricsEnum.UPDATE_VERIFIED_EMAIL        -> metrics.timer("messaging-update-verified-email-timer")
   )
 
   val successCounters = Map(
-    MetricsEnum.SUBSCRIBE                 -> metrics.counter("subscribe-success-counter"),
-    MetricsEnum.REGISTER_WITH_ID_MATCH    -> metrics.counter("messaging-register-with-id-success-counter"),
-    MetricsEnum.REGISTER_WITHOUT_ID       -> metrics.counter("messaging-register-without-id-success-counter"),
-    MetricsEnum.REGISTER_WITH_EORI_AND_ID -> metrics.counter("messaging-register-with-eori"),
-    MetricsEnum.SUBSCRIPTION_STATUS       -> metrics.counter("messaging-subscription-status-success-counter"),
-    MetricsEnum.SUBSCRIPTION_DISPLAY -> metrics.counter(
-      "messaging-subscription-display-success-counter"
-    ),
-    MetricsEnum.REGISTRATION_DISPLAY -> metrics.counter(
-      "messaging-registration-display-success-counter"
-    ),
-    MetricsEnum.VAT_KNOWN_FACTS_CONTROL_LIST -> metrics.counter(
-      "messaging-vat-known-facts-control-list-counter"
-    ),
-    MetricsEnum.UPDATE_VERIFIED_EMAIL -> metrics.counter(
-      "messaging-update-verified-email-success-counter"
-    )
+    MetricsEnum.SUBSCRIBE                    -> metrics.counter("subscribe-success-counter"),
+    MetricsEnum.REGISTER_WITH_ID_MATCH       -> metrics.counter("messaging-register-with-id-success-counter"),
+    MetricsEnum.REGISTER_WITHOUT_ID          -> metrics.counter("messaging-register-without-id-success-counter"),
+    MetricsEnum.REGISTER_WITH_EORI_AND_ID    -> metrics.counter("messaging-register-with-eori"),
+    MetricsEnum.SUBSCRIPTION_STATUS          -> metrics.counter("messaging-subscription-status-success-counter"),
+    MetricsEnum.SUBSCRIPTION_DISPLAY         -> metrics.counter("messaging-subscription-display-success-counter"),
+    MetricsEnum.REGISTRATION_DISPLAY         -> metrics.counter("messaging-registration-display-success-counter"),
+    MetricsEnum.VAT_KNOWN_FACTS_CONTROL_LIST -> metrics.counter("messaging-vat-known-facts-control-list-counter"),
+    MetricsEnum.UPDATE_VERIFIED_EMAIL        -> metrics.counter("messaging-update-verified-email-success-counter")
   )
 
   val failedCounters = Map(
-    MetricsEnum.SUBSCRIBE              -> metrics.counter("subscribe-failed-counter"),
-    MetricsEnum.REGISTER_WITH_ID_MATCH -> metrics.counter("messaging-register-with-id-failed-counter"),
-    MetricsEnum.REGISTER_WITHOUT_ID    -> metrics.counter("messaging-register-without-id-failed-counter"),
-    MetricsEnum.REGISTER_WITH_EORI_AND_ID -> metrics.counter(
-      "messaging-register-with-eori-failed-counter"
-    ),
-    MetricsEnum.SUBSCRIPTION_STATUS -> metrics.counter("messaging-subscription-status-failed-counter"),
-    MetricsEnum.SUBSCRIPTION_DISPLAY -> metrics.counter(
-      "messaging-subscription-display-failed-counter"
-    ),
-    MetricsEnum.REGISTRATION_DISPLAY -> metrics.counter(
-      "messaging-registration-display-failed-counter"
-    ),
+    MetricsEnum.SUBSCRIBE                 -> metrics.counter("subscribe-failed-counter"),
+    MetricsEnum.REGISTER_WITH_ID_MATCH    -> metrics.counter("messaging-register-with-id-failed-counter"),
+    MetricsEnum.REGISTER_WITHOUT_ID       -> metrics.counter("messaging-register-without-id-failed-counter"),
+    MetricsEnum.REGISTER_WITH_EORI_AND_ID -> metrics.counter("messaging-register-with-eori-failed-counter"),
+    MetricsEnum.SUBSCRIPTION_STATUS       -> metrics.counter("messaging-subscription-status-failed-counter"),
+    MetricsEnum.SUBSCRIPTION_DISPLAY      -> metrics.counter("messaging-subscription-display-failed-counter"),
+    MetricsEnum.REGISTRATION_DISPLAY      -> metrics.counter("messaging-registration-display-failed-counter"),
     MetricsEnum.VAT_KNOWN_FACTS_CONTROL_LIST -> metrics.counter(
       "messaging-vat-known-facts-control-list-failed-counter"
     ),
-    MetricsEnum.UPDATE_VERIFIED_EMAIL -> metrics.counter(
-      "messaging-update-verified-email-failed-counter"
-    )
+    MetricsEnum.UPDATE_VERIFIED_EMAIL -> metrics.counter("messaging-update-verified-email-failed-counter")
   )
 
   def startTimer(api: MetricsEnum): Context = timers(api).time()
