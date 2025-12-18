9f28f23b9d69a5ff35099d5cdc1202cafd86afa7
accenture-dami <203887119+accenture-dami@users.noreply.github.com>
2025-12-18 10:49:24 +0000

    Platform upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,16 +7,15 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"              %% "bootstrap-backend-play-30"      % bootstrapVersion,
-    "uk.gov.hmrc"              %% "internal-auth-client-play-30"   % "4.0.0"
+    "uk.gov.hmrc"              %% "internal-auth-client-play-30"   % "4.3.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "org.scalatest"            %% "scalatest"                      % "3.2.19"             % Test,
-    "org.pegdown"              %  "pegdown"                        % "1.6.0"              % Test,
     "org.playframework"        %% "play-test"                      % PlayVersion.current  % Test,
     "org.scalatestplus.play"   %% "scalatestplus-play"             % "7.0.2"              % Test,
     "org.scalatestplus"        %% "mockito-4-6"                    % "3.2.15.0"           % Test,
-    "org.mockito"              %  "mockito-core"                   % "5.20.0"             % Test,
+    "org.mockito"              %  "mockito-core"                   % "5.21.0"             % Test,
     "com.vladsch.flexmark"     %  "flexmark-all"                   % "0.64.8"             % Test,
     "uk.gov.hmrc"              %% "bootstrap-test-play-30"         % bootstrapVersion     % Test
   )
